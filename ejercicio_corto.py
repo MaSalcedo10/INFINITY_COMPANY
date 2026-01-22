ff6ef675506654a87ad6a3295a01e915501857c3
MaSalcedo10 <masg1606@hotmail.com>
2026-01-21 20:26:32 -0500

    Ejercicio Corto

---
+++ ejercicio_corto.py
@@ -0,0 +1,60 @@
+''' Desarrolle una función que reciba una lista de usuarios con nombre, edad, rol, profesión, sueldo y
+retorne solo los usuarios mayores de edad cuyo rol sea empleado o estudiante, ordenados por
+edad de menor a mayor, sueldos de mayor a menor.'''
+
+
+usuario1 = {
+    "nombre": "MIGUEL",
+    "edad": 40,
+    "rol": "Empleado",
+    "profesion": "Ingeniero",
+    "sueldo": 5000
+}
+
+usuario2 = {
+    "nombre": "ZULMA",
+    "edad": 38,
+    "rol": "Empleado",
+    "profesion": "Contadora",
+    "sueldo": 4500
+}
+
+usuario3 = {
+    "nombre": "LUIS",
+    "edad": 17,
+    "rol": "Estudiante",
+    "profesion": "Estudiante",
+    "sueldo": 0
+}
+
+usuario4 = {
+    "nombre": "ABIGAIL",
+    "edad": 16,
+    "rol": "Estudiante",
+    "profesion": "Diseñadora",
+    "sueldo": 3000
+}
+
+usuario5 = {
+    "nombre": "CARLOS",
+    "edad": 45,
+    "rol": "Empleado",
+    "profesion": "Administrador",
+    "sueldo": 7000
+}
+
+lista_usuarios = [usuario1, usuario2, usuario3, usuario4, usuario5]
+
+def filtrar_usuarios():
+    
+    lista_ordenada = []
+    
+    for usuario in lista_usuarios:
+        if usuario["edad"]>=18 and usuario["rol"] in ["Empleado", "Estudiante"]:
+            lista_ordenada.append(usuario)
+            lista_ordenada = sorted(lista_ordenada, key=lambda x: (x["edad"], -x["sueldo"]))
+    
+    for usuario in lista_ordenada:
+        print(usuario)
+    
+filtrar_usuarios()
